b544e95883f0e070799b1fb0f48f075744710f5e
Tejas M R <matblockgreninja@gmail.com>
2021-03-07 11:17:35 +0530

    Initial Commit

---
+++ YoutubePlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubePlayer
+//
+//  Created by tezz on 07/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YoutubePlayer/YoutubePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubePlayerApp.swift
+//  YoutubePlayer
+//
+//  Created by tezz on 07/03/21.
+//
+
+import SwiftUI
+
+@main
+struct YoutubePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
